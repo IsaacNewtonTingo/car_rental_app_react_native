4312e02e52c1623b25e65798679666be105c988a
Isaac Newton Tingo <newtone480@gmail.com>
2022-07-23 22:20:25 +0300

    try

--- app/screens/allRecentlyViewed.js
+++ app/screens/allRecentlyViewed.js
@@ -1,12 +1,18 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import colors from '../components/colors';
 
 export default function AllRecentlyViewed() {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>AllRecentlyViewed</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.background,
+  },
+});
